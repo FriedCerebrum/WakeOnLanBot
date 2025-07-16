9e68b228a41ea617f65cbfd5555cf2f5f02b9f32
FriedCerebrum <iw.kuz8@gmail.com>
2025-07-16 20:13:17 +0300

    7

--- src/handler.rs
+++ src/handler.rs
@@ -5,11 +5,11 @@ use teloxide::dispatching::UpdateFilterExt;
 use crate::{Config, Command};
 
 // Проброс к функциям, определённым в crate:: (main.rs)
-async fn command_bridge(bot: Bot, cfg: Arc<Config>, msg: Message, cmd: Command) -> teloxide::prelude::ResponseResult<()> {
+async fn command_bridge(cfg: Arc<Config>, bot: Bot, msg: Message, cmd: Command) -> teloxide::prelude::ResponseResult<()> {
     crate::command_handler(cfg, bot, msg, cmd).await
 }
 
-async fn callback_bridge(bot: Bot, cfg: Arc<Config>, q: CallbackQuery) -> teloxide::prelude::ResponseResult<()> {
+async fn callback_bridge(cfg: Arc<Config>, bot: Bot, q: CallbackQuery) -> teloxide::prelude::ResponseResult<()> {
     crate::callback_handler(cfg, bot, q).await
 }
 
@@ -18,18 +18,25 @@ pub async fn run(bot: Bot, cfg: Arc<Config>) {
         // Фильтрация по разрешённым пользователям
         .filter(|upd: Update, cfg: Arc<Config>| crate::is_allowed(&cfg, &upd))
         // --- Команды ---
-        .branch(
+        .branch({
+            let cfg = cfg.clone();
             teloxide::filter_command::<Command, _>()
-                .endpoint(command_bridge),
-        )
+                .endpoint(move |bot: Bot, msg: Message, cmd: Command| {
+                    let cfg = cfg.clone();
+                    async move { crate::command_handler(cfg, bot, msg, cmd).await }
+                })
+        })
         // --- CallbackQuery ---
-        .branch(
+        .branch({
+            let cfg = cfg.clone();
             Update::filter_callback_query()
-                .endpoint(callback_bridge),
-        );
+                .endpoint(move |bot: Bot, q: CallbackQuery| {
+                    let cfg = cfg.clone();
+                    async move { crate::callback_handler(cfg, bot, q).await }
+                })
+        });
 
     Dispatcher::builder(bot.clone(), handler)
-        .dependencies(dptree::deps![cfg])
         .enable_ctrlc_handler()
         .build()
         .dispatch()
